2637fb30207a4fa463f837c6c98c96cdeb438b0a
xiangjianwei <1055271311@qq.com>
2024-11-29 18:11:59 +0800

    day: 添加备案跳转2

--- docs/share/utils/listener.ts
+++ docs/share/utils/listener.ts
@@ -1,7 +1,7 @@
 export const listener = {
     copy: () => {
         /** 监听copy事件 */
-        document.addEventListener("copy", function (event: any) {
+        document && document.addEventListener("copy", function (event: any) {
             const selectedText = (window.getSelection() as any).toString();
             const newText = `${selectedText}\n————————————————\n复制什么呢，小心锤你噢[/手动狗头]`;
             event.clipboardData.setData('text/plain', newText);
@@ -11,7 +11,7 @@ export const listener = {
     },
     remove: (name: string) => {
         if (!name) return;
-        document.removeEventListener(name, () => {
+        document && document.removeEventListener(name, () => {
             console.log(`移除${name}完成`);
         });
     },
