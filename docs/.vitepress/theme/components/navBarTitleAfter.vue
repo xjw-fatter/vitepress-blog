98ee2ce9228d0b8aa058d9a3079743a38d2258e1
xiangjianwei <1055271311@qq.com>
2024-12-11 15:43:55 +0800

    day: 文件名

---
+++ docs/.vitepress/theme/components/navBarTitleAfter.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import packageConfig from '../../../../package.json'
+
+</script>
+
+<template>
+  <el-tag text style="vertical-align: middle;margin-left:8px;color: #5e3af2;border-color: #5e3af2;" size="small" effect="plain">{{ packageConfig.version }}</el-tag>
+</template>
